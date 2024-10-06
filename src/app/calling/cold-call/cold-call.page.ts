4258b657d15f77cb30fd1ad7bb76d07c06b06762
satish <msatish080@gmail.com>
2024-10-06 18:57:06 +0530

    added cards

--- src/app/calling/cold-call/cold-call.page.ts
+++ src/app/calling/cold-call/cold-call.page.ts
@@ -9,6 +9,47 @@ export class ColdCallPage implements OnInit {
   selectedClientOption:any;
   selectedCrossSegmentOption:any;
   notLookingOptions:any=['Not Intersted','Voicemail','Call Back','DND']
+  clientTyPeOptions:any=[
+    {
+      id: 1,
+      name: 'HOT',
+      type: 'HOT',
+    },
+    {
+      id: 3,
+      name: 'WARM',
+      type: 'WARM',
+    },
+    {
+      id: 2,
+      name: 'COLD',
+      type: 'COLD',
+    },
+  ];
+  plantoDotypes:any=[
+    {
+      id: 1,
+      name: 'F2F',
+      type: 'F2F',
+    },
+    {
+      id: 2,
+      name: 'Site Visit',
+      type: 'Site Visit',
+    },
+    {
+      id: 3,
+      name: 'Calls',
+      type: 'Calls',
+    },
+    {
+      id: 4,
+      name: 'Closure Meeting',
+      type: 'Closure Meeting',
+    },
+  ];
+  addedProjects:any=[];
+  enterProject:any='';
   constructor() { }
 
   ngOnInit() {
@@ -29,5 +70,32 @@ export class ColdCallPage implements OnInit {
     }
     this.selectedCrossSegmentOption=selectedcrosssegmentbutton;
     console.log(this.selectedClientOption)
+  }
+  compareWith(o1:any, o2:any) {
+    return o1.id === o2.id;
+  }
+
+  handleChange(ev:any) {
+    console.log('Current value:', JSON.stringify(ev.target.value));
+  }
+
+  trackItems(index: number, item: any) {
+    return item.id;
+  }
+  plantodohandleChange(ev:any) {
+    console.log('Current value:', JSON.stringify(ev.target.value));
+  }
+  plantodocompare(o1:any, o2:any) {
+    return o1.id === o2.id;
+  }
+  plantodoitemstracking(index: number, item: any) {
+    return item.id;
+  }
+  addProject(){
+    this.addedProjects.push(this.enterProject);
+    this.enterProject='';
+  }
+  removeproject(val:any){
+    this.addedProjects = this.addedProjects.filter((x:any)=>x != val)
   }
 }
